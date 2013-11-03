7a267b7bc69632b4181da20842003f2a9cbf1cf4
Tristan Sloughter <t@crashfast.com>
2013-11-03 10:21:32 -0600

    first commit

---
+++ src/project_name_sup.erl
@@ -0,0 +1,43 @@
+%%%-------------------------------------------------------------------
+%%% @author {username} <{email}>
+%%% @copyright (C) {year}, {username}
+%%% @doc
+%%%
+%%% @end
+%%% Created : {date} by {username} <{email}>
+%%%-------------------------------------------------------------------
+-module(project_name_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    {ok, {SupFlags, []}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
